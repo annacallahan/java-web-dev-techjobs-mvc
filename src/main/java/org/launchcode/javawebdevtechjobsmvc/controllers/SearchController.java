171b04175e95dbf967bd6554c6ccd647c1b7ecad
annacallahan <anna.r.callahan@gmail.com>
2020-06-08 18:36:27 -0400

    fixed search all with search term bug.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -28,7 +28,7 @@ public class SearchController {
     @PostMapping("/results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
         ArrayList<Job> jobs;
-        if (searchType.toLowerCase().equals("all")) {
+        if (searchType.toLowerCase().equals("all") && searchTerm.equals("")) {
             jobs = JobData.findAll();
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
